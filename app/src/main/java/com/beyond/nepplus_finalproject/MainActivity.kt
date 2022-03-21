b86900d3f12fb6710d23fc7691d3e378f3bab194
MrNubi <99630193+MrNubi@users.noreply.github.com>
2022-03-21 11:08:47 +0900

    #1

--- app/src/main/java/com/beyond/nepplus_finalproject/MainActivity.kt
+++ app/src/main/java/com/beyond/nepplus_finalproject/MainActivity.kt
@@ -2,10 +2,19 @@ package com.beyond.nepplus_finalproject
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.beyond.nepplus_finalproject.inheritClass.BaseActivity
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : BaseActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    override fun SetupEvents() {
+        TODO("Not yet implemented")
+    }
+
+    override fun setValues() {
+        TODO("Not yet implemented")
     }
 }
---
+++ app/src/main/java/com/beyond/nepplus_finalproject/inheritClass/BaseActivity.kt
@@ -0,0 +1,34 @@
+package com.beyond.nepplus_finalproject.inheritClass
+
+import android.content.Context
+import android.os.Bundle
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+
+abstract class BaseActivity: AppCompatActivity() {
+    lateinit var mContext: Context
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        mContext = this
+
+//        val retrfit = ServerAPI.getRetrofit(mContext)
+//        apiList = retrfit.create(APIList::class.java)
+    }
+
+    abstract fun SetupEvents()
+    abstract fun setValues()
+    var mBackWait: Long = 0
+
+    override fun onBackPressed() {
+        // 뒤로가기버튼클릭
+
+
+        if (System.currentTimeMillis() - mBackWait >= 2000) {
+            mBackWait = System.currentTimeMillis()
+            Toast.makeText(baseContext, "한번더누르시면종료됩니다", Toast.LENGTH_SHORT).show()
+        } else {moveTaskToBack(true);
+            finishAndRemoveTask();
+            android.os.Process.killProcess(android.os.Process.myPid());}}
+    }
