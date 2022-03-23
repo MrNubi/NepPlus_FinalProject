8561470ecbc065072166f718c8575ca0f5c92c6f
MrNubi <99630193+MrNubi@users.noreply.github.com>
2022-03-24 01:57:59 +0900

    #7
    대댓글 작성 기능까지 구현
    - 현존 오류: 대댓글 리스트뷰 스크롤이 안움직임 -> 질문 요망

--- app/src/main/java/com/beyond/nepplus_finalproject/adapter/CommentLVAdapter.kt
+++ app/src/main/java/com/beyond/nepplus_finalproject/adapter/CommentLVAdapter.kt
@@ -10,10 +10,12 @@ import androidx.core.view.isVisible
 import com.beyond.nepplus_finalproject.R
 import com.beyond.nepplus_finalproject.data.CommentModel
 import com.beyond.nepplus_finalproject.data.ReReplyModel
+import com.beyond.nepplus_finalproject.util.FBA
 import com.beyond.nepplus_finalproject.util.FBRef
 import com.google.firebase.database.DataSnapshot
 import com.google.firebase.database.DatabaseError
 import com.google.firebase.database.ValueEventListener
+import com.gun0912.tedpermission.provider.TedPermissionProvider.context
 
 
 class CommentLVAdapter(val commentList : MutableList<CommentModel>) : BaseAdapter() {
@@ -39,6 +41,7 @@ class CommentLVAdapter(val commentList : MutableList<CommentModel>) : BaseAdapte
         val title = view?.findViewById<TextView>(R.id.titleArea)
         val time = view?.findViewById<TextView>(R.id.timeArea)
         val btnShow = view?.findViewById<Button>(R.id.btn_reReplyShow)
+        val btnreReply = view?.findViewById<ImageView>(R.id.btn_reReplyPush)
         val LVlayout = view?.findViewById<LinearLayout>(R.id.linlay_LVcover)
         val LVReReply = view?.findViewById<ListView>(R.id.LV_re_comment)
 
@@ -49,6 +52,7 @@ class CommentLVAdapter(val commentList : MutableList<CommentModel>) : BaseAdapte
 
             if (k == true) {
                 LVlayout?.isVisible = false
+                btnShow.text = "댓글 펴기"
             } else {
                 var rereDataList = mutableListOf<ReReplyModel>()
                 var rereAdapter = ReReplyAdapter(rereDataList)
@@ -86,10 +90,43 @@ class CommentLVAdapter(val commentList : MutableList<CommentModel>) : BaseAdapte
                     }
                 }
                 FBRef.re_commentRef.child(commentList[position].key).child(TT).addValueEventListener(postListener)
-
+                LVlayout?.isVisible = true
+                btnShow.text = "댓글 접기"
             }//else
-         LVlayout?.isVisible = true
+
         }//btnClickListener
+
+        btnreReply?.setOnClickListener {
+            var edt_rereply = view?.findViewById<EditText>(R.id.edt_re_replyText)
+            var textReReply = edt_rereply?.text.toString()
+
+            val ttime = commentList[position].commentCreatedTime
+            var ttime2 = ttime.split(".").toString()
+            var ttime3 = ttime.split(".")[0]
+            var ttime4 = ttime.split(".")[1]
+            var ttime5 = ttime.split(".")[2]
+            var ttime6 = ttime5.split(";")[0]
+            var ttime7 = ttime5.split(";")[1].split(":")[0]
+            var ttime8 = ttime5.split(";")[1].split(":")[1]
+
+
+            var TT = "$ttime3$ttime4$ttime5$ttime6$ttime7$ttime8"
+
+            FBRef.re_commentRef
+                .child(commentList[position].key)
+                .child(TT)
+                .push()
+                .setValue(
+                    CommentModel(
+                        textReReply,
+                        FBA.getTime()
+                    )
+                )
+
+            Toast.makeText(context, "댓글 입력 완료", Toast.LENGTH_SHORT).show()
+            edt_rereply?.setText("")
+
+        }
 
         return view!!
     }
